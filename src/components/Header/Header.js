e3dfc8b3c8896afdee377a87d2576d36b65ac64a
Betsimisaraka <anita.ras@onja.org>
2022-06-07 09:15:12 +0300

    Fix the desktop header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components/macro';
 import { Menu, Search, User } from 'react-feather';
 
-import { QUERIES } from '../../constants';
+import { QUERIES, COLORS, FAMILIES } from '../../constants';
 
 import MaxWidthWrapper from '../MaxWidthWrapper';
 import Logo from '../Logo';
@@ -10,7 +10,7 @@ import Button from '../Button';
 
 const Header = () => {
   return (
-    <header>
+    <Wrapper>
       <SuperHeader>
         <Row>
           <ActionGroup>
@@ -28,10 +28,24 @@ const Header = () => {
           </ActionGroup>
         </Row>
       </SuperHeader>
+      <DesctopHeader>
+        <ActionGroup>
+          <button>
+            <Search size={24} />
+          </button>
+          <button>
+            <Menu size={24} />
+          </button>
+        </ActionGroup>
+      </DesctopHeader>
       <MainHeader>
         <Logo />
       </MainHeader>
-    </header>
+      <DesctopHeader>
+        <Button>Subscribe</Button>
+        <Question>Already a subscriber?</Question>
+      </DesctopHeader>
+    </Wrapper>
   );
 };
 
@@ -39,6 +53,10 @@ const SuperHeader = styled.div`
   padding: 16px 0;
   background: var(--color-gray-900);
   color: white;
+
+  @media ${QUERIES.desktopAndUp} {
+    display: none;
+  }
 `;
 
 const Row = styled(MaxWidthWrapper)`
@@ -65,6 +83,32 @@ const MainHeader = styled(MaxWidthWrapper)`
   justify-content: center;
   margin-top: 32px;
   margin-bottom: 48px;
+`;
+
+const DesctopHeader = styled.div`
+  display: none;
+
+  @media ${QUERIES.desktopAndUp} {
+    display: block;
+  }
+`;
+
+const Wrapper = styled(MaxWidthWrapper)`
+
+  @media ${QUERIES.desktopAndUp} {
+    display: flex;
+    align-items: center;
+  }
+`
+
+const Question = styled.p`
+  text-decoration-line: underline;
+  color: ${COLORS.gray[700]};
+  padding-top: 0.5rem;
+  font-family: ${FAMILIES.serif};
+  font-size: 14px;
+  line-height: 22px;
+  text-align: center;
 `;
 
 export default Header;
